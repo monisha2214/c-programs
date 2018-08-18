363b0b2d033c6c7e2806c897c5ef5f1431eebe91
monisha <monishasrini2214@gmail.com>
2018-08-18 21:35:43 +0530

    modified program to vlidate whether choice is correct or not with if condition

--- addition_and_subtraction_using_switch.c
+++ addition_and_subtraction_using_switch.c
@@ -3,7 +3,10 @@ int main () {
 int choice, firstNumber, secondNumber, result;
 printf("1. Addition\n2. Subtraction\nPlease enter your choice:");
 scanf("%d", &choice);
-printf("entered choice is %d\n", &choice);
+if(choice > 0 || choice <= 2) {
+    printf("Yes you have entered a valid choice\n");
+    printf("Your entered choice is %d\n", &choice);
+}
 printf("please enter two numbers:");
 scanf("%d %d", &firstNumber, &secondNumber);
 switch (choice) {
